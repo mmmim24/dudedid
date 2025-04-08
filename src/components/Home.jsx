2024605348346cc73dd001b2ce4d5ee0e1ea267b
mmmim24 <mustaq24@student.sust.edu>
2025-04-08 13:55:06 +0600

    state conditional device view component render

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
+import { TaskStore } from '../store/taskStore';
 import MobileView from './MobileView';
 import WebViw from './WebViw';
 
 const Home = () => {
+    const { state, dispatch } = React.useContext(TaskStore);
+    const [isMobile, setIsMobile] = React.useState(true);
+    const handleResize = () => {
+        if (window.innerWidth > 1024) {
+            setIsMobile(false);
+        } else {
+            setIsMobile(true);
+        }
+    }
+    state.sort((priorityA, priorityB) => {
+        const priorityOrder = ['High', 'Medium', 'Low'];
+        return priorityOrder.indexOf(priorityA.priority) - priorityOrder.indexOf(priorityB.priority);
+    });
+    React.useEffect(() => {
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, [])
     return (
         <>
-            <div className='hidden lg:block'>
-                <WebViw />
-            </div>
-            <div className='lg:hidden'>
-                <MobileView />
-            </div>
+            {
+                isMobile ? <MobileView ctx={{ state, dispatch }} /> : <WebViw ctx={{ state, dispatch }} />
+            }
         </>
     )
 }
