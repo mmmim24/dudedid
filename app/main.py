e37d86c802b3584384635951c92f093175dd2a9b
Mustaq Mujahid Mim <mustaq24@student.sust.edu>
2026-01-01 23:09:57 +0600

    default route

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-print('Hello')
+# cd app
+# source ./venv/bin/activate
+# uvicorn main:app --reload
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello from slowAPI"}
+
+
+# python3 ./main.py
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+
+# deactivate
